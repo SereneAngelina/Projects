307f563710e85aa55c399f30fce7f233ba4982cb
SereneAngelina <sereneangelina1@gmail.com>
2017-09-01 20:46:03 +0200

    Adding dinosaur program

---
+++ app/models/dinosaur.rb
@@ -0,0 +1,16 @@
+class Dinosaur < ApplicationRecord
+
+  validates :name, presence: true
+  validates :name, uniqueness: true
+
+  validates :age, length: { maximum: 5 }, allow_blank: true
+  validates :image_url, length: { maximum: 255 }, allow_blank: true
+
+  def self.order_by_name
+    order(:name)
+  end
+
+  def is_baby?
+    age < 4
+  end
+end
